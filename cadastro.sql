28a6e0b0e640a979d0fd5870c2c72d3c68e3156d
ewanuel <emanuelmedeirosrosa@outlook.com>
2023-09-06 22:44:09 -0300

    php2

---
+++ cadastro.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE cadastro;
+
+USE cadastro;
+
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    sobrenome VARCHAR(255) NOT NULL,
+    cpf CHAR(14) NOT NULL,
+    cep CHAR(9) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    senha VARCHAR(255) NOT NULL
+);
