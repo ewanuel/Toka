d0835d5c486a720ccc8770af0deb7a7ce7ebfe10
ewanuel <emanuelmedeirosrosa@outlook.com>
2023-09-09 20:09:46 -0300

    php4

--- crie_sua_conta.php
+++ crie_sua_conta.php
@@ -9,8 +9,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (empty($nome) || empty($sobrenome) || empty($cpf) || empty($cep) || empty($email) || empty($senha)) {
         echo "Por favor, preencha todos os campos.";
-    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    }elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         echo "Email inválido.";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL) || !preg_match("/@(outlook\.com|gmail\.com)$/", $email)) {
+        echo "Por favor, use um email do tipo @outlook.com ou @gmail.com.";
     } elseif (!preg_match("/^\d{3}\.\d{3}\.\d{3}-\d{2}$/", $cpf)) {
         echo "CPF inválido.";
     } elseif (!preg_match("/^\d{5}-\d{3}$/", $cep)) {
@@ -33,6 +35,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             die("Falha na conexão com o banco de dados: " . $conn->connect_error);
         }
 
+         // Verifica se o email já está cadastrado
+         $email_exists_query = "SELECT email FROM usuarios WHERE email = ?";
+         $stmt_check_email = $conn->prepare($email_exists_query);
+         $stmt_check_email->bind_param("s", $email);
+         $stmt_check_email->execute();
+         $stmt_check_email->store_result();
+ 
+         if ($stmt_check_email->num_rows > 0) {
+             echo "Este email já está cadastrado, coloque outro";
+             exit;
+         }
+
+        //criptografia
+        $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
+        $cpf_hash = password_hash($cpf, PASSWORD_DEFAULT);
+
         // Prepara a instrução SQL para inserção
         $sql = "INSERT INTO usuarios (nome, sobrenome, cpf, cep, email, senha) VALUES (?, ?, ?, ?, ?, ?)";
 
@@ -43,7 +61,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             echo "Erro na preparação da consulta.";
         } else {
             // Faz o bind dos parâmetros
-            $stmt->bind_param("ssssss", $nome, $sobrenome, $cpf, $cep, $email, $senha);
+            $stmt->bind_param("ssssss", $nome, $sobrenome, $cpf_hash, $cep, $email, $senha_hash);
 
             // Executa a instrução SQL
             if ($stmt->execute()) {
